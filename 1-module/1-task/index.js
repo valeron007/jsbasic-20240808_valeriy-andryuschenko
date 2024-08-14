455c169a867aa59755224e43735aa2b0f7027bfd
valeron <valeronchik0702@gmail.com>
2024-08-14 21:12:54 +0300

    added function factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  if(n < 0) return NaN
+  if(n === 0 ) return 1;
+  if(n === 1 ) return 1;
+
+  return n * factorial(n - 1);
 }
+
+let fact = factorial(5)
+console.log(fact);

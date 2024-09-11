09b954eb5e537f9be7d02b56dfc6160ad42ea3b5
valeron <valeronchik0702@gmail.com>
2024-09-11 13:37:52 +0300

    done tasks 2 and 3

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,78 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+  elem = null
+  title = null
+  body = null
   constructor() {
+    this.elem = this.#render();
+
   }
+
+  #template() {
+    return `
+        <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    this.title = this.elem.querySelector('.modal__title');
+    this.body = this.elem.querySelector('.modal__body');
+
+    let buttonClosed = this.elem.querySelector('.modal__close');
+    buttonClosed.addEventListener('click', this.close);
+
+    document.addEventListener('keydown', this.closeKeyDown);
+
+    return this.elem;
+  }
+
+  open() {
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open')
+  }
+
+  setTitle(title = '') {
+    this.title.textContent = title;
+  }
+
+  setBody(element) {
+    this.body.innerHTML = '';
+    this.body.append(element);
+  }
+
+  closeKeyDown = (event) => {
+    if (event.code === 'Escape') {
+      this.close();
+      document.removeEventListener('keydown', this.closeKeyDown);
+    }
+  }
+  close = (event) => {
+    document.body.classList.remove('is-modal-open');
+    this.elem.remove();
+    document.removeEventListener('keydown', this.closeKeyDown);
+  }
+
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,83 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class StepSlider {
+  elem = null
+  #steps = 0
+  #value = 0
+  sliderParams = 0
   constructor({ steps, value = 0 }) {
+    this.#steps = steps;
+    this.#value = value;
+    this.#render();
   }
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    this.elem.addEventListener('click', this.#sliderChange);
+
+    return this.elem;
+  }
+
+  #sliderChange = (event) => {
+    this.sliderParams = this.elem.getBoundingClientRect();
+    let left = event.clientX - this.sliderParams.left;
+    let leftRelative = left / this.elem.offsetWidth;
+    let segments = this.#steps - 1;
+    let approximateValue = leftRelative * segments;
+    this.#value = Math.round(approximateValue);
+    let valuePercents = this.#value / segments * 100;
+
+    let removeActiveSpan = this.elem.querySelector('.slider__step-active');
+    removeActiveSpan.classList.remove('slider__step-active');
+
+    let spans = [...this.elem.getElementsByTagName('span')];
+    spans[this.#value].classList.toggle('slider__step-active');
+
+    let sliderTumb = this.elem.querySelector('.slider__thumb');
+    sliderTumb.style.setProperty('left', `${valuePercents}%`);
+
+    let sliderProgress = this.elem.querySelector('.slider__progress');
+    sliderProgress.style.setProperty('width', `${valuePercents}%`)
+
+    const sliderChangeEvent = new CustomEvent("slider-change", {
+      detail: this.#value,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(sliderChangeEvent);
+  }
+
+  #template() {
+    return `
+        <!--Корневой элемент слайдера-->
+          <div class="slider">
+            <!--Ползунок слайдера с активным значением-->
+            <div class="slider__thumb">
+              <span class="slider__value">${this.#value}</span>
+            </div>
+
+            <!--Заполненная часть слайдера-->
+<!--            <div class="slider__progress" style="width: 10%;"></div>-->
+            <div class="slider__progress" style="width: 0%;"></div>
+
+            <!--Шаги слайдера-->
+            <div class="slider__steps">
+              ${this.generateSliderStep()}
+            </div>
+          </div>
+    `;
+  }
+
+  generateSliderStep(){
+    let templateSlider = '';
+    for (let step = 0; step < this.#steps; step++) {
+      if (step === this.#value) {
+        templateSlider += '<span class="slider__step-active"></span>';
+        continue;
+      }
+      templateSlider += '<span></span>';
+    }
+    return templateSlider;
+  }
+
 }
